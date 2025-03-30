8253d9aa93955e5e8218c5bacc8a54660e81b4a6
Vani Sahithi Kakani <sahithi@MacBookPro.lan>
2025-03-30 16:52:45 +0530

    Added a dropdown for recently searched cities

--- src/script.js
+++ src/script.js
@@ -1,12 +1,15 @@
 const apiKey = "9dff6354e13424cceeb90f570e993fcc";
+const dropdown = document.getElementById("recentDropdown");
+const cityInput = document.getElementById("cityName");
+const recentCitiesList = document.getElementById("recentCitiesList");
 
-const weatherAnimation = document.getElementById("weatherAnimation");
 async function getWeather() {
-  let city = document.getElementById("cityName").value.trim();
+  let city = cityInput.value.trim();
   if (!city) {
     alert("enter a city name");
   }
   fetchWeather(city);
+  saveToRecentCities(city);
 }
 function getWeatherByCurrentLocation() {
   if (navigator.geolocation) {
@@ -46,10 +49,45 @@ function displayWeather(data) {
 
   document.getElementById(
     "weatherResult"
-  ).innerHTML = `<p><strong>${data.name}</strong></p>
+  ).innerHTML = `<div><p><strong>${data.name}</strong></p>
   <p>Temperatue: ${data.main.temp}&deg;C</p>
   <p>Wind: ${data.wind.speed}M/S</p>
-  <p>Humidity: ${data.main.humidity}%</p>
+  <p>Humidity: ${data.main.humidity}%</p></div>
   <img src="${iconUrl}" alt="${weatherCondition}" class="w-20 h-20 mx-auto">
 `;
 }
+
+function saveToRecentCities(city) {
+  let cities = JSON.parse(localStorage.getItem("recentCities")) || [];
+  if (!cities.includes(city)) {
+    cities.push(city);
+    localStorage.setItem("recentCities", JSON.stringify(cities));
+  }
+}
+
+cityInput.addEventListener("input", () => {
+  let cities = JSON.parse(localStorage.getItem("recentCities")) || [];
+  let typedcity = cityInput.value.toLowerCase();
+
+  let matchingCities = cities.filter((city) =>
+    city.toLowerCase().startsWith(typedcity)
+  );
+
+  recentCitiesList.innerHTML = "";
+
+  if (matchingCities.length > 0) {
+    matchingCities.forEach((city) => {
+      let listitem = document.createElement("li");
+      listitem.textContent = city;
+      listitem.className = "text-black pr-4 h-10 border";
+      listitem.addEventListener("click", () => {
+        cityInput.value = city;
+        dropdown.classList.add("hidden");
+      });
+      recentCitiesList.append(listitem);
+    });
+    dropdown.classList.remove("hidden");
+  } else {
+    dropdown.classList.add("hidden");
+  }
+});
